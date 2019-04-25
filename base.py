a7e872052a942ad5cba0d58582516c1659c298b6
Guilherme Cardoso <gvcardoso@hotmail.com>
2019-04-25 11:48:08 -0300

    Todos os commits vao ser atualizados

--- base.py
+++ base.py
@@ -10,14 +10,14 @@ PATH_BBOX = 'WeaponS_bbox'
 obj = utl.Tools()
 
 # Function
-# obj.xml_to_csv(path_data=PATH_DATA, path_bbox=PATH_BBOX)
+obj.xml_to_csv(path_data=PATH_DATA, path_bbox=PATH_BBOX)
 
 dir_arquivo = os.path.join(os.getcwd(), PATH_DATA, 'images.txt')
 
 for line in os.listdir(PATH_DATA):
-  if not os.path.exists(dir_arquivo):
-    with open(dir_arquivo, 'w+') as out_arq:
-      out_arq.write(os.path.join(os.getcwd(), PATH_DATA, line))
-  else:
-    with open(dir_arquivo, 'a+') as out_arq:
-      out_arq.write('\n' + os.path.join(os.getcwd(), PATH_DATA, line))
+    if not os.path.exists(dir_arquivo):
+      with open(dir_arquivo, 'w+') as out_arq:
+            out_arq.write(os.path.join(os.getcwd(), PATH_DATA, line))
+    else:
+        with open(dir_arquivo, 'a+') as out_arq:
+            out_arq.write('\n' + os.path.join(os.getcwd(), PATH_DATA, line))
